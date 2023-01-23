76302db7866c13dae111930b7508ebf95df2d0ce
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-23 23:18:17 +0530

    initial commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import products from "../Products";
+import products from "../ProductData";
 const Home = () => {
      return (
           <>
