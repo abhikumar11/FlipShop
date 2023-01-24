206355cb37c4897fce4ffc0d1813d85d5efc2036
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-24 23:33:57 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,22 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
      return (
-          <>
-               <Header />
-               <main className="my-3">
-                    <Container>
-                         <h1>Flip Shop</h1>
-                         <Home />
-                    </Container>
-               </main>
-               <Footer />
-          </>
+          <BrowserRouter>
+                    <Header />
+                    <main className="my-3">
+                         <Container>
+                              <h1>Flip Shop</h1>
+                              <Routes>
+                                   <Route path="/" element={<Home />} exact />
+                              </Routes>
+                         </Container>
+                    </main>
+                    <Footer />
+               
+          </BrowserRouter>
      );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
      return (
           <>
                <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
                     <Container>
-                         <Navbar.Brand>Flip Shop</Navbar.Brand>
+                         <LinkContainer to="/">
+                              <Navbar.Brand>Flip Shop</Navbar.Brand>
+                              </LinkContainer>
                          <Navbar.Toggle aria-controls="basic-navbar-nav" />
                          <Navbar.Collapse id="basic-navbar-nav">
                               <Nav className="ml-auto">
+                                   <LinkContainer to="/cart">
                                    <Nav.Link>
-                                   <i class="fa-solid fa-cart-shopping"></i>
-                                    &nbsp;Cart
-                                    </Nav.Link>
-                                   <Nav.Link>
-                                   <i class="fa-solid fa-user"></i>
-                                   &nbsp;Account
+                                        <i class="fa-solid fa-cart-shopping"></i>
+                                        &nbsp;Cart
                                    </Nav.Link>
+                                   </LinkContainer>
+                                   <LinkContainer to="/account">
+                                   <Nav.Link>
+                                        <i class="fa-solid fa-user"></i>
+                                        &nbsp;Account
+                                   </Nav.Link>
+                                   </LinkContainer>
                               </Nav>
                          </Navbar.Collapse>
                     </Container>
