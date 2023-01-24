dc9d804cf22ab2c9c611035d486f784a6c24253f
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-24 23:38:08 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,26 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
 function App() {
      return (
           <BrowserRouter>
-                    <Header />
-                    <main className="my-3">
-                         <Container>
-                              <h1>Flip Shop</h1>
-                              <Routes>
-                                   <Route path="/" element={<Home />} exact />
-                              </Routes>
-                         </Container>
-                    </main>
-                    <Footer />
-               
+               <Header />
+               <main className="my-3">
+                    <Container>
+                         <h1>Flip Shop</h1>
+                         <Routes>
+                              <Route path="/" element={<Home />} exact />
+                         </Routes>
+                         <Routes>
+                              <Route
+                                   path="/product/:id"
+                                   element={<ProductDetail />}
+                              />
+                         </Routes>
+                    </Container>
+               </main>
+               <Footer />
           </BrowserRouter>
      );
 }
---
+++ client/src/components/ProductDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Product from './Product';
+const ProductDetail = () => {
+  return (
+    <div>ProductDetail</div>
+  )
+}
+
+export default ProductDetail;
