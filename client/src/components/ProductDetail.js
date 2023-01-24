2b5d14817f54772868ecc898bf09b6c04cd7e2f0
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-25 00:10:32 +0530

    initial commit

--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import Product from './Product';
+import products from "../ProductData";
+import {Row,Col,Button,ListGroup,Image, ListGroupItem} from "react-bootstrap";
+import { useParams } from 'react-router-dom';
 const ProductDetail = () => {
+    const {id}=useParams();
+    const product=products.find((i)=>i._id===id)
   return (
-    <div>ProductDetail</div>
+    <div>
+        <Row>
+            <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+            <Col md={3}>
+                   <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            
+                        </ListGroupItem>
+                   </ListGroup>
+            </Col>
+        </Row>
+    </div>
   )
 }
 
