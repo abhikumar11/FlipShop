d4ec1667e7d26a2827f76a7c35a6ccb04b61430e
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-25 18:42:58 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-const express=require('express');
-const app=express();
-const port=5000;
+const express = require("express");
+const app = express();
+const port = 5000;
+const product = require("./ProductData");
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
-app.get("/products",(req,res)=>{
-    res.send("Hello World");
-})
-app.listen(port,()=>{
-    console.log(`Example app listening on port ${port}`);
+app.get("/", (req, res) => {
+     res.send("Hello World");
+});
+
+app.get("/products", (req, res) => {
+     res.json(product);
+});
+
+app.get("/products/:id", (req, res) => {
+ const pro=product.product.find(e=>e._id===req.params.id)
+ res.json(pro);
+});
+app.listen(port, () => {
+     console.log(`Example app listening on port ${port}`);
 });
