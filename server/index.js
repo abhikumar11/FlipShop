c9b1eeee00f3569d024cbb9c1c92a5a6645e291f
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-26 11:24:11 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const dotenv=require('dotenv');
 const app = express();
-const port = 5000;
 const product = require("./ProductData");
 
+dotenv.config();
+const port=process.env.PORT;
 app.get("/products", (req, res) => {
      res.json(product.product);
 });
@@ -11,6 +13,7 @@ app.get("/product/:id", (req, res) => {
  const pro=product.product.find(e=>e._id===req.params.id)
  res.json(pro);
 });
+
 app.listen(port, () => {
      console.log(`Example app listening on port ${port}`);
 });
