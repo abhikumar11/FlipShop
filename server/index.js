41e250248d5ecaf4c06092e0adcc8163cb17ffef
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-26 14:57:15 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,20 @@ const dotenv=require('dotenv');
 const app = express();
 const product = require("./ProductData");
 const mongoose = require('mongoose');
+
+const connectDb=async()=>
+{
+     try {
+         const conn=await mongoose.connect(process.env.CONNECTITON_URL); 
+         console.log('Connected to database',conn.connection.host);
+     } catch (error) {
+         console.error(error.message); 
+         process.exit(1);
+     }
+}
 dotenv.config();
+mongoose.set('strictQuery',false);
+connectDb();
 const port=process.env.PORT;
 app.get("/products", (req, res) => {
      res.json(product.product);
