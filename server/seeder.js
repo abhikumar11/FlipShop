2d4dfc87801f8b6ea2d2107c67b9f938c3f2ee1f
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-27 15:29:58 +0530

    initial commit

--- server/seeder.js
+++ server/seeder.js
@@ -8,3 +8,27 @@ const product = require('./ProductData');
 const connectDb  = require('./DatabaseConnection');
 dotenv.config();
 
+const insertData=async()=>{
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+        const newUser=await User.insertMany(Users);
+        const admin=newUser[0]._id
+        const data=product.product.map((item)=>{
+            return {...item,User:admin}
+        })
+        await Product.insertMany(data);
+        console.log("data inserted");
+        process.exit();
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+};
+const deleteData=async()=>{
+  
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+};
