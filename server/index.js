7a1c260cf93256c519dc48932183878cdc776dd1
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-28 17:57:43 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const products = require("./ProductData");
 const mongoose = require('mongoose');
 const connectDb=require('./DatabaseConnection');
 const ProductRouter=require("./routes/ProductRoutes");
+const{errorHandler}=require("./messages/ErrorMessage");
 dotenv.config();
 connectDb();
 const port=process.env.PORT;
@@ -13,7 +14,7 @@ app.get('/',(req,res)=>{
      res.send('<h1>Welcome</h1>');
 })
 app.use(ProductRouter);
-
+app.use(errorHandler);
 
 app.listen(port, () => {
      console.log(`Example app listening on port ${port}`);
