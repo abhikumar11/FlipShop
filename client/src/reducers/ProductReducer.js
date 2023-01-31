42477f60d680bf2165f52bc68b47885ef4495d8d
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-31 21:28:41 +0530

    initial commit

--- client/src/reducers/ProductReducer.js
+++ client/src/reducers/ProductReducer.js
@@ -1,19 +1,36 @@
 import {
      PRODUCT_DETAILS_FAILS,
+     PRODUCT_DETAILS_REQUEST,
+     PRODUCT_DETAILS_SUCCESS,
      PRODUCT_LIST_FAILS,
      PRODUCT_LIST_REQUEST,
      PRODUCT_LIST_SUCCESS,
 } from "../constants/ProductConstants";
 
-export const productReducer = (state = {products:[]}, action) => {
+export const productReducer = (state = { products: [] }, action) => {
      switch (action.type) {
           case PRODUCT_LIST_REQUEST:
-            return { loading: true, products: [] };
+               return { loading: true, products: [] };
           case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: action.payload };
+               return { loading: false, products: action.payload };
           case PRODUCT_LIST_FAILS:
-            return { loading: false, error: action.payload };
+               return { loading: false, error: action.payload };
           default:
-            return state;
-        }
+               return state;
+     }
+};
+export const getProductReducer = (
+     state = { product: { reviews: [] } },
+     action
+) => {
+     switch (action.type) {
+          case PRODUCT_DETAILS_REQUEST:
+               return { loading: true, ...state };
+          case PRODUCT_DETAILS_SUCCESS:
+               return { loading: false, product: action.payload };
+          case PRODUCT_DETAILS_FAILS:
+               return { loading: false, error: action.payload };
+          default:
+               return state;
+     }
 };
