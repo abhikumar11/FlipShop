a002b4d4f538fc21b0e70baad11ff60e0a17ae38
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-02-05 10:44:14 +0530

    initial commit

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {useDispatch,useSelector} from "react-redux";
 import {addToCart} from "../actions/CartAction";
-import {useNavigate, useParams} from "react-router-dom"
+import {useNavigate,useParams,useSearchParams} from "react-router-dom"
 const Cart = () => {
   const history =useNavigate();
-  const param=useParams();
-  const id=param.id;
-  const qty=param.qty;
+  const{id}=useParams();
+  const [param] = useSearchParams();
+  const qty=Number(param.get('qty'));
   return (
     <div>Cart</div>
   )
