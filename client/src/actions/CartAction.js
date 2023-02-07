28d18f606fc9022a5f0a0ee9758a83f4c8d1c878
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-02-07 18:18:26 +0530

    initial commit

--- client/src/actions/CartAction.js
+++ client/src/actions/CartAction.js
@@ -17,3 +17,6 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
+ export const removeFromCart=(id)=>async(dispatch, getState)=>{
+    
+};
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../actions/CartAction";
+import { addToCart, removeFromCart } from "../actions/CartAction";
 import {
      Link,
      useNavigate,
