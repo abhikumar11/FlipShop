6e1fdd016bd0c88190cef932f0b83369e0f4aad5
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-02-07 17:25:36 +0530

    initial commit

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import {addToCart} from "../actions/CartAction";
-import {useNavigate,useParams, useSearchParams} from "react-router-dom"
+import {Link, useNavigate,useParams, useSearchParams} from "react-router-dom"
+import{Row,Col,Form,Button,Card,Image,ListGroup,ListGroupItem} from "react-bootstrap";
+import Message from './Message';
 const Cart = () => {
   const history =useNavigate();
   const{id}=useParams();
@@ -13,11 +15,29 @@ const Cart = () => {
   }, [dispatch,id,qty]);
   
   const cart = useSelector((state) => state.cart);
-  const{item} =cart;
+  const item =cart;
   console.log(item);
   
   return (
-    <div>Cart</div>
+    <>
+    <Row>
+      <Col md={8}>
+      <h1>Shopping Cart</h1>
+      {
+      cart.length === 0?(
+      <Message>
+        Your Cart is Empty !<Link to="/">Start Shopping</Link>
+        </Message>
+        ):(
+          <ListGroup variant='flush'>
+
+          </ListGroup>
+        )
+      }
+      </Col>
+
+    </Row>
+    </>
   )
 }
 
