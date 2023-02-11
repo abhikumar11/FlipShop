f121d6f61c4ace8bd670567f58d438724c69ea0d
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-02-11 09:28:22 +0530

    initial commit

--- server/controllers/UserControlller.js
+++ server/controllers/UserControlller.js
@@ -1,18 +1,18 @@
 
 const User = require("../schema/UserSchema");
 const asyncHandler = require("express-async-handler");
-
+const generateToken=require("../tokens/GenerateToken");
 const authController=asyncHandler(async (req, res) => {
   const{email,password}=res.body;
    const user=await User.findOne({email});
    if(user&&(await user.matchPassword(password)))
    {
     res.json({
-      _id:User._id,
-      name:User.name,
-      email:User.email,
-      isAdmin:User.isAdmin,
-      tocken:null,
+      _id:user._id,
+      name:user.name,
+      email:user.email,
+      isAdmin:user.isAdmin,
+      tocken:generateToken(user._id),
     })
    }
    else
@@ -21,4 +21,9 @@ const authController=asyncHandler(async (req, res) => {
         throw new Error("Invalid username or password");
    }
 })
-module.exports =authController;
+
+const getUser=asyncHandler(async (req, res) => {
+  res.send("Sucessfully logged in");
+})
+
+module.exports ={authController,getUser};
