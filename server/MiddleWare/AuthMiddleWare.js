d258a9d8d2396aae4449144ffda1bce520a7d458
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-02-12 11:23:20 +0530

    initial commit

--- server/MiddleWare/AuthMiddleWare.js
+++ server/MiddleWare/AuthMiddleWare.js
@@ -1,11 +1,20 @@
 const jwt=require("json-web-token");
 const User=require("../schema/UserSchema");
-
-const protect=async(req,res,next)=>
+const asyncHandler=require("express-async-handler");
+const protect=asyncHandler((req,res,next)=>
 {
     let token;
-    console.log(req.headers.authorization);
-    next();
-}
+    if(req.headers.authorization.startsWith("Bearer"))
+    try {
+        token=req.headers.authorization.split(" ")[1];
+        const decode=jwt.verify(token,process.env.JWT_KEY);
+        next();
+    } catch (error) {}
+    if(!token)
+    {
+        res.status(401);
+        throw new Error("Not authorized");
+    }
+});
 
 module.exports=protect;
