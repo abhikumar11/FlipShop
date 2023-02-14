f46d0cb7f612b014279eb2721fed2e9d9982a679
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-02-14 16:34:50 +0530

    initial commit

--- client/src/actions/UserAction.js
+++ client/src/actions/UserAction.js
@@ -4,6 +4,9 @@ import {
      USER_LOGIN_REQUEST,
      USER_LOGIN_SUCCESS,
      USER_LOGOUT,
+     USER_REGISTER_FAIL,
+     USER_REGISTER_REQUEST,
+     USER_REGISTER_SUCCESS,
 } from "../constants/UserConstants";
 
 export const userLogin = (email, password) => async (dispatch) => {
@@ -30,7 +33,28 @@ export const userLogin = (email, password) => async (dispatch) => {
           });
      }
 };
-export const userLogout = () =>dispatch=>{
-  localStorage.removeItem('userinfo');
-  dispatch({type: USER_LOGOUT});
-}
+export const userLogout = () => (dispatch) => {
+     localStorage.removeItem("userinfo");
+     dispatch({ type: USER_LOGOUT });
+};
+export const userRegister = (name, email, password) => async (dispatch) => {
+     try {
+          dispatch({ type: USER_REGISTER_REQUEST });
+          const config = { headers: { "Content-Type": "application/json" } };
+          const { data } = await axios.post(
+               "/user",
+               { name, email, password },
+               config
+          );
+          dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+          localStorage.setItem('userinfo', JSON.stringify(data));
+     } catch (error) {
+          dispatch({
+               type: USER_REGISTER_FAIL,
+               payload:
+                    error.response && error.response.data.message
+                         ? error.response.data.message
+                         : error.message,
+          });
+     }
+};
--- client/src/reducers/UserReducer.js
+++ client/src/reducers/UserReducer.js
@@ -3,6 +3,9 @@ import {
      USER_LOGIN_SUCCESS,
      USER_LOGIN_FAIL,
      USER_LOGOUT,
+     USER_REGISTER_REQUEST,
+     USER_REGISTER_SUCCESS,
+     USER_REGISTER_FAIL
 } from "../constants/UserConstants";
 
 export const userReducer = (state = {}, action) => {
@@ -20,3 +23,15 @@ export const userReducer = (state = {}, action) => {
         }
 };
 
+export const registerReducer = (state = {}, action) => {
+  switch (action.type) {
+       case USER_REGISTER_REQUEST:
+         return { loading: true };
+       case USER_REGISTER_SUCCESS:
+         return { loading: false, userinfo: action.payload };
+       case USER_REGISTER_FAIL:
+         return { loading: false, error: action.payload };
+       default:
+         return state;
+     }
+};
